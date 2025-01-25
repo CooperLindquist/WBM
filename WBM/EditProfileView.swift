6952b6462bec738ee04001dfe8c8d6907a3be60c
Cooper Lindquist <90310805@ep-student.org>
2025-01-24 18:13:31 -0600

    I dont even know tbh

---
+++ WBM/EditProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  EditProfileView.swift
+//  WBM
+//
+//  Created by Cooper Lindquist on 1/24/25.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditProfileView()
+}
